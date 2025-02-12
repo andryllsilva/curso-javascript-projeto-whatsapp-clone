c549f7a0766abfff64499cfa05ed3cf11231c291
Andryll Silva <andryll.silva@saipos.com>
2025-02-12 11:28:38 -0300

    135. W24 - Criando evento para o visualizar o tempo de gravação

--- src/controller/MicrophoneController.js
+++ src/controller/MicrophoneController.js
@@ -69,6 +69,7 @@ export class MicrophoneController extends ClassEvent{
       })
 
       this._mediaRecorder.start();
+      this.startTimer();
       
     }
   }
@@ -79,6 +80,21 @@ export class MicrophoneController extends ClassEvent{
       
       this._mediaRecorder.stop();
       this.stop();
+      this.stopTimer();
     }
+  }
+
+  startTimer(){
+    let start = Date.now()
+    
+        this._recordMicrophoneInterval = setInterval(() => {
+    
+          this.trigger('recordtimer', (Date.now() - start))
+        }, 100)
+  }
+
+  stopTimer(){
+
+    clearInterval(this._recordMicrophoneInterval);
   }
 }
