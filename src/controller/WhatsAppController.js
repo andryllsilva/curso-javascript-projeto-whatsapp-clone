d2a214e92f3c48bc8cccd37adcb5ddc40b473cc3
Andryll Silva <andryll.silva@saipos.com>
2025-02-12 17:03:41 -0300

    141. W30 - Adicionando um contato no Whatsapp Clone

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -170,7 +170,7 @@ export class WhatsAppController {
       this._user.name = this.el.inputNamePanelEditProfile.innerHTML;
 
       this._user.save().then(() => {
-        
+
         this.el.btnSavePanelEditProfile.disabled = false;
 
       });
@@ -190,6 +190,22 @@ export class WhatsAppController {
       e.preventDefault();
 
       let formData = new FormData(this.el.formPanelAddContact);
+
+      let contact = new User(formData.get('email'));
+
+      contact.on('datachange', data => {
+        
+        if(data.name) {
+          this._user.addContact(contact).then(() => {
+            this.el.btnClosePanelAddContact.click();
+            console.info('Contato foi adicionado')
+          })
+        }else {
+          console.error('Usuário não foi encontrado')
+        }
+      })
+
+      this._user.addContact();
     })
 
     this.el.contactsMessagesList.querySelectorAll('.contact-item').forEach(item => {
