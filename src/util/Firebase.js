0d4bd00f33a3119129a4a657c04d07fb41c942ba
Andryll Silva <andryll.silva@saipos.com>
2025-02-12 13:19:40 -0300

    136. W25 - Configurando o Firebase no projeto

---
+++ src/util/Firebase.js
@@ -0,0 +1,40 @@
+const firebase = require('firebase');
+require('firebase/firestore');
+
+export class Firebase {
+
+  constructor() {
+    this._config = {
+      apiKey: "AIzaSyDzvYT_5oPm6jH9c3-3wqsYb_8ib8Htmqs",
+      authDomain: "whats-clone-andryll.firebaseapp.com",
+      projectId: "whats-clone-andryll",
+      storageBucket: "whats-clone-andryll.firebasestorage.app",
+      messagingSenderId: "749662237621",
+      appId: "1:749662237621:web:e2b54376be5fc9d3271ffc",
+      measurementId: "G-H3PJJ7D2K0"
+    }
+
+    this.init();
+  }
+
+  init() {
+
+    if(!this.initialized){
+      firebase.initializeApp(this._config)
+
+      firebase.firestore().settings({
+        timestampsInSnapshots: true
+      })
+
+      this.initialized = true;
+    }
+  }
+
+  static db(){
+    return Firebase.firestore();
+  }
+
+  static hd(){
+    return Firebase.storage()
+  }
+}
