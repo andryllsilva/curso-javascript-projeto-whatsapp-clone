06d416c4054fea9528febfc66091a8d23bdac9ab
Andryll Silva <andryll.silva@saipos.com>
2025-02-12 11:20:48 -0300

    134. W23 - Gravando áudio capturado pelo microfone com MediaRecorder

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -292,18 +292,20 @@ export class WhatsAppController {
 
       this._microphoneController = new MicrophoneController();
 
-      this._microphoneController.on('play', audio => {
+      this._microphoneController.on('ready', audio => {
         console.log('receba', audio)
+
+        this._microphoneController.startRecorder()
       })
     })
 
     this.el.btnCancelMicrophone.on('click', e => {
-      this._microphoneController.stop();
+      this._microphoneController.stopRecorder();
       this.closeRecordMicrophone()
     })
 
     this.el.btnFinishMicrophone.on('click', e => {
-      this._microphoneController.stop();
+      this._microphoneController.stopRecorder();
       this.closeRecordMicrophone()
     })
 
