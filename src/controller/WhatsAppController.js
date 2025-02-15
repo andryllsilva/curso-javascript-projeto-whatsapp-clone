1685e6d8afe1649fe71e0486b4b8a7b54bef74be
Andryll Silva <andryll.silva@saipos.com>
2025-02-15 15:39:30 -0300

    153. W42 - Enviando Documento como anexo

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -6,6 +6,7 @@ import { Firebase } from './../util/Firebase'
 import { User } from './../model/User'
 import { Chat } from './../model/Chat'
 import { Message } from './../model/Message'
+import { Base64 } from "../util/base64";
 
 
 export class WhatsAppController {
@@ -216,7 +217,14 @@ export class WhatsAppController {
           this.el.panelMessagesContainer.appendChild(view)
 
 
-        } else if (me) {
+        } else {
+
+          let view = message.getViewElement(me)
+          this.el.panelMessagesContainer.querySelector('#_' + data.id).innerHTML = view.innerHTML;
+
+        }
+        
+        if (this.el.panelMessagesContainer.querySelector('#_' + data.id) && me) {
           let msgEl = this.el.panelMessagesContainer.querySelector('#_' + data.id)
 
           msgEl.querySelector('.message-status').innerHTML = message.getStatusViewElement().outerHTML
@@ -492,26 +500,26 @@ export class WhatsAppController {
         context.drawImage(picture, 0, 0, canvas.width, canvas.height)
 
         fetch(canvas.toDataURL(mimeType))
-        .then(res => { return res.arrayBuffer()})
-        .then(buffer => { return new File([buffer], filename, { type: mimeType})})
-        .then(file => {
+          .then(res => { return res.arrayBuffer() })
+          .then(buffer => { return new File([buffer], filename, { type: mimeType }) })
+          .then(file => {
 
-          Message.sendImage(this._contactActive.chatId, this._user.email, file)
+            Message.sendImage(this._contactActive.chatId, this._user.email, file)
 
-          this.el.btnSendPicture.disabled = false;
+            this.el.btnSendPicture.disabled = false;
 
-          this.closeAllMainPanel();
-          this._camera.stop();
-          this.el.btnReshootPanelCamera.hide();
-          this.el.pictureCamera.hide()
-          this.el.videoCamera.show()
-          this.el.containerSendPicture.hide()
-          this.el.containerTakePicture.show()
-          this.el.panelMessagesContainer.show();
-        })
+            this.closeAllMainPanel();
+            this._camera.stop();
+            this.el.btnReshootPanelCamera.hide();
+            this.el.pictureCamera.hide()
+            this.el.videoCamera.show()
+            this.el.containerSendPicture.hide()
+            this.el.containerTakePicture.show()
+            this.el.panelMessagesContainer.show();
+          })
       }
 
-      
+
     })
 
     this.el.btnReshootPanelCamera.on('click', e => {
@@ -586,7 +594,28 @@ export class WhatsAppController {
     })
 
     this.el.btnSendDocument.on('click', e => {
-      console.log('send document')
+
+
+      let file = this.el.inputDocument.files[0]
+      let base64 = this.el.imgPanelDocumentPreview.src;
+
+      if (file.type === 'application/pdf') {
+
+        Base64.toFile(base64).then(filePreview => {
+          Message.sendDocument(
+            this._contactActive.chatId,
+            this._user.email, file, base64, this.el.infoPanelDocumentPreview.innerHTML);
+  
+        })
+      } else {
+        Message.sendDocument(
+          this._contactActive.chatId,
+          this._user.email, file);
+      }
+
+      this.el.btnClosePanelDocumentPreview.click()
+
+
     })
 
     this.el.btnAttachContact.on('click', e => {
